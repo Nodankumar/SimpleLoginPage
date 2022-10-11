a70085ee68a41a3036d66a327a9292b6b9289b8f
NodanKumar <93375753+Grogu-dev@users.noreply.github.com>
2022-10-11 11:29:46 +0530

    Update login.js

--- 1SimpleLoginPage/login.js
+++ 1SimpleLoginPage/login.js
@@ -2,7 +2,7 @@ function validate(){
   var username=document.getElementById('username').value;
   var password=document.getElementById('password').value;
 
-  if(username=='reddynodankumar@gmail.com' && password=='nodankumar@'){
+  if(username=='Grogu_dev@gmail.com' && password=='Grogu_dev'){
     location.href='../HomePage.html';
   }
   else{
