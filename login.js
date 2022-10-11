f4ae0a54fa8ed17acb72df4a9459735b7aa02457
NodanKumar <93375753+Grogu-dev@users.noreply.github.com>
2022-10-11 11:52:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,10 +3,10 @@ function validate(){
   var password=document.getElementById('password').value;
 
   if(username=='Grogu_dev@gmail.com' && password=='Grogu_dev'){
-    location.href=url('https://grogu-dev.github.io/Flipkart-Clone/');
+    location.href='https://grogu-dev.github.io/Flipkart-Clone/';
   }
   else{
-    location.href=url('https://grogu-dev.github.io/SimpleLoginPage/');
+    location.href='https://grogu-dev.github.io/SimpleLoginPage/);
     alert('Enter wrong credentials..')
   }
 
