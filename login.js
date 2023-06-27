250ab5e0663046caa89f44c82aae3104962be17d
Reddy.Nodankumar <reddynodankumar@gmail.com>
2023-06-27 15:53:42 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function validate(){
   var password=document.getElementById('password').value;
 
   if(username=='Grogu_dev@gmail.com' && password=='Grogu_dev'){
-    location.href='https://grogu-dev.github.io/Flipkart-Clone/';
+    location.href='https://nodankumar.github.io/Flipkart-Clone/';
   }
   else{
     location.href='https://grogu-dev.github.io/SimpleLoginPage/';
